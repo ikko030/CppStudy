0db68130e1c21eb7a605404b27d9989c3ed5bffa
JI EUN CHEON <154047155+ikko030@users.noreply.github.com>
2024-05-01 11:04:52 +0000

    파일 정리 및 pointer기초 자료 추가

---
+++ C++class/basicPointer/pointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//포인터는 실행 중 메모리의 주소 값이다
+//"포인터 변수란 주소를 저장하는 변수다!
+//포인터 변수를 선언하는 연산자는 *다!
+//&연산자는 실행중인 주소 값을 알아낸다!
+
+
+int main() {
+
+	int n = 3;
+
+	int* p;
+	p = &n;					
+
+	cout << p << endl;      //n의 주소값
+	cout << *p << endl;     //n값
+
+
+	return 0;
+}
